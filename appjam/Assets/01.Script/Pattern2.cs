a370342806f20f56484698211dbe8ed1e871d3d3
준1 <98933515+Wizone06@users.noreply.github.com>
2024-04-20 21:38:09 +0900

    찐 수정 끝
    찐 수정 끝

--- appjam/Assets/01.Script/Pattern2.cs
+++ appjam/Assets/01.Script/Pattern2.cs
@@ -1,32 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Pattern2 : MonoBehaviour
 {
-    [SerializeField]
-    GameObject laser;
-    [SerializeField]
-    GameObject warning;
-    GameObject target;
-    public GameObject Effect;
+    [SerializeField] private GameObject laser;
+    [SerializeField] private GameObject warning;
+    [SerializeField] private GameObject effectPrefab; // ¿Ã∆Â∆Æ «¡∏Æ∆’
+    [SerializeField] private int maxEffects = 3; // √÷¥Î ª˝º∫ ¿Ã∆Â∆Æ ºˆ
 
-    Vector3 vec;
-
-    float length;
+    private GameObject target;
+    private Vector3 vec;
+    private float length;
 
     private void Start()
     {
         target = GameObject.Find("target").gameObject;
-
         vec = (transform.position - target.transform.position).normalized;
-
-        set();
-
+        SetRotation();
         StartCoroutine(Warning());
     }
+
     private void Update()
     {
-        if(laser.activeSelf)
+        if (laser.activeSelf)
         {
             Quaternion quater = transform.rotation;
             Vector2 startPos = transform.position;
@@ -37,46 +35,44 @@ public class Pattern2 : MonoBehaviour
             {
                 if (hitinfos.collider != null)
                 {
-                    if (hitinfos.collider.gameObject.CompareTag("Player"))
+                    if (hitinfos.collider.gameObject.CompareTag("Player") )
                     {
                         length = (hitinfos.collider.gameObject.transform.position - this.gameObject.transform.position).sqrMagnitude;
                         laser.transform.localScale = new Vector3(0.2f, Mathf.Sqrt(length) * 2, this.gameObject.transform.localScale.y);
-                        dieEffectShow(hitinfos.collider.gameObject);
                         break;
                     }
-                    else if (hitinfos.collider.gameObject.CompareTag("target"))
+                    if (hitinfos.collider.gameObject.CompareTag("target"))
                     {
                         length = (hitinfos.collider.gameObject.transform.position - this.gameObject.transform.position).sqrMagnitude;
                         laser.transform.localScale = new Vector3(0.2f, Mathf.Sqrt(length) * 2, this.gameObject.transform.localScale.y);
-                        dieEffectShow(hitinfos.collider.gameObject);
+                        ShowkillEffect(hitinfos.collider.gameObject);
                         break;
                     }
                 }
             }
         }
     }
-    public void dieEffectShow(GameObject obj)
+
+    private void ShowkillEffect(GameObject obj)
     {
-        GameObject effct = Instantiate(Effect, obj.transform.position, Quaternion.identity);
-        Destroy(effct, 1.5f);
+        GameObject effect = Instantiate(effectPrefab, obj.transform.position, Quaternion.identity);
+        Destroy(effect, 0.5f);
     }
 
-
-    private void set()
+    private void SetRotation()
     {
         float angle = Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg;
-
         laser.SetActive(false);
-
-        transform.rotation = Quaternion.Euler(0, 0,-90 + angle);
-
+        transform.rotation = Quaternion.Euler(0, 0, -90 + angle);
         this.gameObject.transform.SetParent(target.transform);
     }
+
     private IEnumerator Warning()
     {
         yield return new WaitForSeconds(1.5f);
         StartCoroutine(Laser());
     }
+
     private IEnumerator Laser()
     {
         warning.SetActive(false);
@@ -85,4 +81,6 @@ public class Pattern2 : MonoBehaviour
         laser.SetActive(false);
         Destroy(this.gameObject);
     }
+
+   
 }
--- appjam/Assets/01.Script/Player/Snake/tail/BodyPart.cs
+++ appjam/Assets/01.Script/Player/Snake/tail/BodyPart.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,6 +7,9 @@ public class BodyPart : MonoBehaviour
     public float rotationSpeed = 5f; // 회전 속도
     public UnityEvent PreventEvent;
     private bool isMoving = true;
+    private bool hasCollided = false; // 충돌 여부를 나타내는 변수
+    [SerializeField] private GameObject effectPrefab; // 이펙트 프리팹
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -17,7 +21,7 @@ public class BodyPart : MonoBehaviour
         {
             isMoving = true;
         }
-        if (isMoving)
+        if (isMoving && !hasCollided) // 충돌하지 않은 상태에서만 회전하도록 조건 추가
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePosition.z = 0f;
@@ -25,8 +29,25 @@ public class BodyPart : MonoBehaviour
             Quaternion lookRotation = Quaternion.LookRotation(Vector3.forward, direction);
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
         }
-        
+
     }
 
-   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("laser") && !hasCollided) // 충돌하지 않은 상태에서만 이펙트 생성하도록 조건 추가
+        {
+            Instantiate(effectPrefab, transform.position, Quaternion.identity);
+            hasCollided = true; // 충돌 상태로 변경
+
+            // 0.5초 후에 다시 충돌 여부를 false로 변경하는 코루틴 시작
+            StartCoroutine(ResetCollision());
+        }
+    }
+
+    // 충돌 상태를 재설정하는 코루틴
+    IEnumerator ResetCollision()
+    {
+        yield return new WaitForSeconds(0.5f);
+        hasCollided = false; // 충돌 상태를 false로 재설정
+    }
 }
