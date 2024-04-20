8baaec4fd4a93417ef50f0d4554f6dfb1f1b6a98
준1 <98933515+Wizone06@users.noreply.github.com>
2024-04-21 01:46:48 +0900

    ㅁㄴㅇ
    ㅁㄴㅇ

--- appjam/Assets/01.Script/TextAttack.cs
+++ appjam/Assets/01.Script/TextAttack.cs
@@ -7,16 +7,15 @@ public class TextAttack : MonoBehaviour
 {
 
     [SerializeField] private GameObject effectPrefab; // 이펙트 프리팹
-
     private GameObject target; // 타겟
+    private bool isMoving = true; // 이동 여부를 나타내는 플래그
+    private float moveSpeed = 10f; // 이동 속도
 
     // 텍스트가 생성될 때 호출됩니다.
     private void Start()
     {
         // 타겟 설정
         SetTarget();
-        // 타겟으로 텍스트 보내기
-        StartCoroutine(MoveTextToTarget());
     }
 
     // 타겟 설정 메서드
@@ -30,35 +29,24 @@ public class TextAttack : MonoBehaviour
         }
     }
 
-    // 타겟으로 텍스트 보내는 코루틴
-    private IEnumerator MoveTextToTarget()
+    // 업데이트에서 플레이어 위치로 이동
+    private void Update()
     {
-        if (target != null)
+        if (isMoving && target != null)
         {
-            Vector3 startPosition = transform.position;
             Vector3 targetPosition = target.transform.position;
-            float journeyLength = Vector3.Distance(startPosition, targetPosition);
-            float startTime = Time.time;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
 
-            while (transform.position != targetPosition)
+            if (transform.position == targetPosition)
             {
-                float distanceCovered = (Time.time - startTime) * 10; // 이동 속도를 조정할 수 있습니다.
-                float fractionOfJourney = distanceCovered / journeyLength;
-                transform.position = Vector3.Lerp(startPosition, targetPosition, fractionOfJourney);
-                yield return null;
+                isMoving = false;
+                Destroy(gameObject, 2.5f); // 텍스트를 파괴합니다.
             }
-
-            // 이펙트 생성 및 삭제
-            GameObject effect = Instantiate(effectPrefab, targetPosition, Quaternion.identity);
-            Destroy(effect, 0.5f);
-
-            // 텍스트를 파괴합니다.
-            Destroy(gameObject);
         }
         else
         {
-            // 타겟이 없는 경우 경고 출력
-            Debug.LogWarning("No target available!");
+            // 타겟이 없는 경우나 이동이 완료된 경우 경고 출력
+            Debug.LogWarning("No target available or already reached the target!");
         }
     }
 
@@ -67,7 +55,7 @@ public class TextAttack : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             // 플레이어에 닿으면 이 텍스트를 삭제하고 플레이어에게 이펙트를 생성합니다.
-            Destroy(gameObject);
+            Destroy(this.gameObject);
             ShowEffect(other.transform.position);
         }
         else if (other.CompareTag("target"))
